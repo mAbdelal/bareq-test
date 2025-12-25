ca413d31de55c97c61a851dcb96031376d315c81
MajdiAbdelal <majdy.moh.hos@gmail.com>
2025-12-25 10:07:10 +0200

    first commit

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const corsOptions = {
     origin: NODE_ENV === 'production'
         ? CLIENT_URL
-        : 'https://bareq-frontend-test-ab4z.vercel.app',
+        : 'https://bareq-frontend-test-ab4z-mzaq9tfxh-majdis-projects-3748a949.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
