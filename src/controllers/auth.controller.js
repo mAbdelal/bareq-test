049b9ebc6a445a25e7cceb8056a4d6958a1a2aeb
MajdiAbdelal <majdy.moh.hos@gmail.com>
2025-12-25 20:08:30 +0200

    first commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -616,21 +616,21 @@ async function handleGoogleCallback(req, res, next) {
 
         res.cookie('accessToken', accessToken, {
             httpOnly: true,
-            secure: NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: parseTimeToMilliseconds(JWT_ACCESS_EXPIRES_IN),
         });
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
-            secure: NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: parseTimeToMilliseconds(JWT_REFRESH_EXPIRES_IN),
         });
 
         res.cookie('userPayload', JSON.stringify(tokenPayload), {
             httpOnly: false,
-            secure: NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure:true,
+            sameSite: 'none',
             maxAge: parseTimeToMilliseconds(JWT_REFRESH_EXPIRES_IN),
         });
 
