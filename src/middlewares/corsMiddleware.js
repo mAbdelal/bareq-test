66cd9cb75e9cca6f8dca54e4545a80064ad34ad0
MajdiAbdelal <majdy.moh.hos@gmail.com>
2026-02-14 19:27:35 +0200

    first commit

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -8,13 +8,7 @@ const allowedOrigins = [
 
 const corsOptions = {
     origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+        callback(null, true);
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'],
     allowedHeaders: ['Content-Type', 'Authorization'],
